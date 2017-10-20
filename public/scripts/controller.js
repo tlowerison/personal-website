2282a942c36a6c805ae56de43f420d499388beb6
Trey Lowerison <tlowerison@berkeley.edu>
2017-10-20 05:03:09 -0700

    Finished!

--- public/scripts/controller.js
+++ public/scripts/controller.js
@@ -4,6 +4,58 @@ angular.module("MyApp", [])
 	$locationProvider.hashPrefix("");
 })
 .controller("Ctrl", function($scope, $http) {
+	// Responsive Text
+		var size = $(window).width() > 768 ? 'sm' : 'md';
+		var triggers = {
+			'#name': [
+				[
+					'font-size', 
+					{
+						'down': '2.8rem',
+						'up': '4rem'
+					}
+				]
+			],
+			'#style-link': [
+				[
+					'font-size', 
+					{
+						'down': '2rem',
+						'up': '2rem'
+					}
+				]
+			]
+		}
+
+		function trigger(dir) {
+			if (dir == 'down') {
+				return size == 'md' && $(window).width() < 768;
+			} else if (dir == 'up') {
+				return size == 'sm' && $(window).width() >= 768;
+			}
+		}
+		function prepTrigger(dir) {
+			size = (dir == "up" ? "sm" : "md");
+		}
+		function resize() {
+			var triggered = trigger('up') ? 'up' : (trigger('down') ? 'down' : 'none');
+			var nextTrigger = triggered == 'done' ? 'none' : (triggered == 'up' ? 'down' : 'up');
+			if (triggered != 'none') {
+				for (var key in triggers) {
+					for (var index in triggers[key]) {
+						var propVal = triggers[key][index];
+						var cssObj = {};
+						cssObj[propVal[0]] = propVal[1][triggered];
+						$(key).css(cssObj);
+					}
+				}
+				prepTrigger(nextTrigger);
+			}
+		}
+
+		$(window).on("resize", resize);
+
+	// Text Animation, Background SVG Cache Trick
 	$(document).ready(function() {
 		$('#name').animate({
 			opacity: 1,
@@ -20,32 +72,6 @@ angular.module("MyApp", [])
 		var backgroundImage = $('#pic').css('background-image').split("=")[0] + "=" + (10000000 * Math.random()).toString();
 		$('#pic').css({'background-image': backgroundImage});
 
-		var size = $(window).width() > 768 ? 'sm' : 'md';
 		resize();
 	});
-	var size = $(window).width() > 768 ? 'sm' : 'md';
-	function trigger(dir) {
-		if (dir == 'down') {
-			return size == 'md' && $(window).width() < 768;
-		} else if (dir == 'up') {
-			return size == 'sm' && $(window).width() >= 768;
-		}
-	}
-	function prepTrigger(dir) {
-		size = (dir == "up" ? "sm" : "md");
-	}
-
-	function resize() {
-		console.log(size);
-		if (trigger('down')) {
-			$('#name').css({'font-size': '3rem'});
-			$('#style-link').css({'font-size': '2rem'});
-			prepTrigger('up');
-		} else if (trigger('up')) {
-			$('#name').css({'font-size': '4rem'});
-			$('#style-link').css({'font-size': '2rem'});
-			prepTrigger('down');
-		}
-	}
-	$(window).on("resize", resize);
 });
