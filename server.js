275456cb8074a76514dfb135e624d025de19bbff
Trey Lowerison <tlowerison@berkeley.edu>
2017-10-19 22:46:36 -0700

    removed image link in svg, changed to image link in div

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var dotenv = require('dotenv');
 dotenv.load();
 
 var app = express();
-app.set('port', process.env.PORT || 3018);
+app.set('port', process.env.PORT || 3001);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Production error handler
@@ -16,6 +16,10 @@ if (app.get('env') === 'production') {
 		res.sendStatus(err.status || 500);
 	});
 }
+
+app.get('/img/mountains.svg?lastmod=', function(req, res) {
+	res.sendFile('/public/img/mountains.png');
+});
 
 app.listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
